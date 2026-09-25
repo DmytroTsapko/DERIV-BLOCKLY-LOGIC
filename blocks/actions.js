5517e59197c1e86f971f5180f7e8a7d16442dc0d
DmytroTsapko <132236214+DmytroTsapko@users.noreply.github.com>
2026-09-25 16:09:20 +0300

    feat: add logic core structure

---
+++ blocks/actions.js
@@ -0,0 +1,10 @@
+Blockly.common.defineBlocksWithJsonArray([
+  {
+    type: "logic_action",
+    message0: "Action = %1",
+    args0: [{ type: "field_dropdown", name: "ACTION", options: [["FALL", "FALL"], ["RISE", "RISE"]] }],
+    previousStatement: null,
+    nextStatement: null,
+    colour: 120
+  }
+]);
