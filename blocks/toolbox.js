285d37968e86a54413bdeec7693bb5b4d6c3080d
DmytroTsapko <132236214+DmytroTsapko@users.noreply.github.com>
2026-09-25 16:09:22 +0300

    feat: add logic core structure

---
+++ blocks/toolbox.js
@@ -0,0 +1,8 @@
+window.LogicToolbox = {
+  kind: "flyoutToolbox",
+  contents: [
+    { kind: "block", type: "logic_if" },
+    { kind: "block", type: "candle_condition" },
+    { kind: "block", type: "logic_action" }
+  ]
+};
