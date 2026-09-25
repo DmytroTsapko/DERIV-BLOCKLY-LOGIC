ce8126d774ebf56433c705aadd73b2c280e2b72e
DmytroTsapko <132236214+DmytroTsapko@users.noreply.github.com>
2026-09-25 16:09:57 +0300

    feat: complete v0.2 logic core

--- blocks/conditions.js
+++ blocks/conditions.js
@@ -1,5 +1,15 @@
 Blockly.common.defineBlocksWithJsonArray([
   {
+    type: "logic_if",
+    message0: "IF %1 DO",
+    args0: [{ type: "input_value", name: "CONDITION", check: "Boolean" }],
+    message1: "%1",
+    args1: [{ type: "input_statement", name: "ACTION" }],
+    previousStatement: null,
+    nextStatement: null,
+    colour: 65
+  },
+  {
     type: "candle_condition",
     message0: "Last Candle = %1",
     args0: [{ type: "field_dropdown", name: "DIR", options: [["DOWN", "DOWN"], ["UP", "UP"]] }],
