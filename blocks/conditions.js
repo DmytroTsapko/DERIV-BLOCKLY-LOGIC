1d182792e8375b4523daaa018e8971b20529a741
DmytroTsapko <132236214+DmytroTsapko@users.noreply.github.com>
2026-09-25 16:09:18 +0300

    feat: add logic core structure

---
+++ blocks/conditions.js
@@ -0,0 +1,9 @@
+Blockly.common.defineBlocksWithJsonArray([
+  {
+    type: "candle_condition",
+    message0: "Last Candle = %1",
+    args0: [{ type: "field_dropdown", name: "DIR", options: [["DOWN", "DOWN"], ["UP", "UP"]] }],
+    output: "Boolean",
+    colour: 210
+  }
+]);
